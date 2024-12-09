d9889c94e563e61b36d91eec013c3009725a4056
Md Samsuddoha <mdsamsuddoha@Mds-MacBook-Pro-2.local>
2024-12-09 22:10:06 +0600

    L3

---
+++ Lecture 3/Code/division.py
@@ -0,0 +1,20 @@
+
+# a=10
+# b=5
+# c=a/b
+# print(c)
+
+number1 = float(input("Enter Number 1: "))
+number2=input("Enter number 2:")
+number2=float(number2)
+result=number1/number2
+print(f"The division is: {result}")
+
+# number1 = float(input("Enter Number 1: "))
+# number2=input("Enter number 2:")
+# number2=float(number2)
+# if number2==0:
+#     print("Infinite")
+# else:
+#     result=number1/number2
+#     print(f"The division is: {result}")
---
+++ Lecture 3/Code/problem1.py
@@ -0,0 +1,11 @@
+#Write a python program that show the following output
+# Hello, I am [you name], I am [your age] years old, and I am from [your city].
+
+#user input
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+city=input('Enter your city: ')
+
+#f-string
+print(f"Hello, I am {name}, I am {age} years old, and I am from {city}")
+
